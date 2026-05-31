ef3d6fcbc878048c4f3300fd8d60f63a8af878f4
Thang Nguyen <minhthang0403@gmail.com>
2026-05-31 11:41:24 +0700

    feat: scaffold yourssh_script_engine package

---
+++ packages/yourssh_script_engine/lib/yourssh_script_engine.dart
@@ -0,0 +1,8 @@
+library yourssh_script_engine;
+
+export 'src/hook_bus.dart';
+export 'src/plugin_manifest.dart';
+export 'src/permission_guard.dart';
+export 'src/plugin_ui_registry.dart';
+export 'src/script_engine_service.dart';
+export 'src/plugin_loader.dart';
